032cc1c64b6061546e49ed84a5e45289ccf95cb5
rails_project <a1722yz@aiit.ac.jp>
2019-06-19 16:01:52 +0900

    EarthModel

---
+++ Optional/EarthModel.java
@@ -0,0 +1,12 @@
+public class EarthModel {
+	private String lastName = null;
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String strLastName) {
+		this.lastName = strLastName;	
+	}
+
+}
