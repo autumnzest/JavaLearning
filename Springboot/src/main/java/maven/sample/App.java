3332d4f30560c7cbd32ee6e0096311e356f0bcf0
rails_project <a1722yz@aiit.ac.jp>
2020-02-20 10:51:52 +0900

    Create App.java

---
+++ Springboot/src/main/java/maven/sample/App.java
@@ -0,0 +1,16 @@
+package maven.sample;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("maven.sample.sqlmap")
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
